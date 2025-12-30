66b08c7f0051a64ca49db4a836312ddaab8f4306
UmerKhan1998 <devUk9298@gmail.com>
2025-12-30 11:12:08 +0500

    push

--- uptime-worker.js
+++ uptime-worker.js
@@ -9,6 +9,11 @@ const MONITORED_URLS = [
   "https://school-journey-d80d29443185.herokuapp.com",
   "https://server-monitoring-004f594d63b2.herokuapp.com",
   "https://quran-journey-staging-4e781e6719ce.herokuapp.com/ping",
+  "https://api.heroku1.com/apps/your-app-name-1/6546546",
+  "https://server-monitoring-004f594d63b2.herokuapp.com",
+  "https://quran-journey-staging-4e781e6719ce.herokuapp.com/ping",
+  "https://server-monitoring-004f594d63b2.herokuapp.com/",
+  "https://api.heroku.com/apps/your-app-name-2",
 ];
 
 const transporter = nodemailer.createTransport({
